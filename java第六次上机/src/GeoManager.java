c6ab8af773e56fc6fe38c6ecbdf57a3a9a598a3f
PrpgramTraveler <2115655391@qq.com>
2020-11-12 16:06:31 +0800

    2020-11-12

--- java第六次上机/src/GeoManager.java
+++ java第六次上机/src/GeoManager.java
@@ -1,7 +1,12 @@
+import java.io.*;
 import java.util.*;
 
+import static java.util.Collections.sort;
+
 public class GeoManager {
+    //记录图形对象
     private List<GeometricObject> graph=new ArrayList<GeometricObject>();
+    //使用hashmap容器，按照图形名和图形对象数组的形式储存
     private Map<String,ArrayList<GeometricObject>> map=new HashMap<String,ArrayList<GeometricObject>>();
 
     //将图形对象存入容器
@@ -19,32 +24,84 @@ public class GeoManager {
         }
         System.out.println();
     }
+
     //对各图形进行分类
-
     public void classification(){
+        //遍历图形容器
         for(int i=0;i<graph.size();i++){
+            //将对应图形名的容器传给temp对象数组
             ArrayList<GeometricObject> temp=map.get((graph.get(i)).Name());
-
+            //检查这个容器是否为空
             if(temp!=null){
+                //如果不为空
                 //System.out.println("no null");
+                //将图形对象加入到这个容器
                 temp.add((graph.get(i)));
-                //Collections.sort(temp);
+                //对容器中的对象按照len的大小进行排序
+                temp.sort(Comparator.comparing(GeometricObject::Len));
             }else{
-
+                //如果这个容器为空，那么新建一个容器，把对象放入到这个容器中
                 temp=new ArrayList<GeometricObject>();
                 temp.add(graph.get(i));
 
             }
-            System.out.println((graph.get(i)).Name()+":"+temp.size());
-
+            //System.out.println((graph.get(i)).Name()+":"+temp.size());
+            //把图形名和改变后的对象数组放进map中
             map.put((graph.get(i)).Name(),temp);
-
         }
     }
-    public void Sort(){
-        for(int i=0;i<graph.size();i++){
-            
+
+    //显示map中图形名称，以及排序后的边长
+    public void show() throws IOException {
+        //这个map来按照图形名和它的边长来保存
+        Map<String,ArrayList<Integer>> TempM=new HashMap<String,ArrayList<Integer>>();
+        //获得map中的key值
+        Set<String>set= map.keySet();
+        //按key中来遍历
+        for(String str : set){
+            //System.out.println(map.get((graph.get(i))));
+            //记录图形的边长容器
+            ArrayList<Integer> temp=new ArrayList<Integer>();
+            //获得相应的图形名中的对应的对象容器
+            ArrayList<GeometricObject> TempO=map.get(str);
+
+            //System.out.println(TempO.size());
+            //遍历这个对象容器
+            for(int j=0;j<TempO.size();j++){
+                //记录这个对象容器中对应的边长
+                temp.add((TempO.get(j)).Len());
+            }
+            //更新map容器
+            TempM.put(str,temp);
         }
+        //将文件信息写入文件
+        Writer writer=new FileWriter("E:\\JAVA\\java第六次上机\\src\\1.txt");
+        writer.write(String.valueOf(TempM));
+        //释放资源
+        writer.close();
+        System.out.println(TempM);
+
+    }
+
+    public void Read() throws IOException {
+        //读文件信息
+        Reader reader=new FileReader("E:\\JAVA\\java第六次上机\\src\\1.txt");
+        int len;
+        //字符数组
+        char[] ch=new char[20];
+        while((len=reader.read(ch))!=-1){
+            //将读取到的类容转换成字符串，打印
+            /*
+                ch:表示要操作的数组
+                0:表示起始索引
+                len:表示要操作的字符的个数
+             */
+            String s=new String(ch,0,len);
+            System.out.print(s);
+
+        }
+
+        reader.close();
     }
 
 }
--- java第六次上机/src/Screen.java
+++ java第六次上机/src/Screen.java
@@ -1,17 +1,18 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Screen {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         //定义String数组。方便使用随机数来生成数组的下标
         String[] graph={"圆形","矩形","三角形","多边形"};
         //创建一个随机数生成器
         Random ran=new Random();
         //创建GeoManager对象来管理这10个图形
         GeoManager GM=new GeoManager();
-        //生产10个随机图形
-        for(int i=0;i<10;i++){
+        //生产11个随机图形
+        for(int i=0;i<11;i++){
             //产生0-3的随机数
             int temp=ran.nextInt(4);
             int temp_len=ran.nextInt(10)+1;
@@ -21,15 +22,20 @@ public class Screen {
             //将对象放入容器
             GM.Add(GT);
         }
+
         //查看移除之前容器中的图形
         GM.check();
         //移出指定位置的图形
-        GM.remove(1);
+        GM.remove(10);
         //检查移除之后容器中的图形
         GM.check();
-
+        //对图形进行分类
         GM.classification();
-
+        //显示图形信息
+        GM.show();
+        //读文件信息
+        System.out.println("读出文件");
+        GM.Read();
 
 
 
